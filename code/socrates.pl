3da395059a93b047f14c659d5683785a928f5dfa
Evgeny Cherkashin <eugene@irnok.net>
2015-01-05 18:44:45 +0800

    Finished the exaple part of the problem solvig section. Added the definition of the problem area.

--- code/socrates.pl
+++ code/socrates.pl
@@ -1,9 +1,15 @@
 h(s).
 m(X):-h(X).
 
-   e(a,b). e(b,c). e(c,f).
-   e(f,e). e(e,d). e(d,h).
-   e(f,g). e(g,i). e(i,h).
+e(a,b). e(b,c). e(c,f).
+e(f,e). e(e,d). e(d,h).
+e(f,g). e(g,i). e(i,h).
 
-    path(A,B) :- e(A,B).
-    path(A,B) :- e(A,C), path(C,B).
+path(A,B) :- e(A,B).
+path(A,B) :- e(A,C), path(C,B).
+
+path(A,B, [A-B]) :- e(A,B).
+path(A,B, [A-C|T]) :- e(A,C), path(C,B,T).
+
+path2(A,B, [A,B]) :- e(A,B).
+path2(A,B, [A|T]) :- e(A,C), path2(C,B,T).
