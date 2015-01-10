04b3b55321ae7a44984edde92cf97e80f7f6e69b
Evgeny Cherkashin <eugene@irnok.net>
2015-01-10 15:41:39 +0800

    First version of uniform-cost like algorithm implementation.

--- code/rzd.pl
+++ code/rzd.pl
@@ -36,14 +36,12 @@ disconn:-
 
 
 
-route(A,B, Z,Dist):-
-        station(A,_,CA,_),
-        write('A:'),write(CA),nl,
-        dist1(СA,СB, Z,_),
-        write('B:'),write(CB),nl,
-        shortest(CA,CB,Node,Dist),
-        station(B,_,CB,_),
-        station(Z,_,Node,_).
+% route2(A,B, Z,Dist):-
+%         station(A,_,CA,_),
+%         dist1(СA,СB, Z,_),
+%         shortest(CA,CB,Node,Dist),
+%         station(B,_,CB,_),
+%         station(Z,_,Node,_).
 
 
 dist1(A,B, Node,Dist):-
@@ -58,3 +56,86 @@ shortest(A,B, Node,Dist):-
         keysort(L, [Dist-Node|_]),!.
 
 % 920002 Тайшет
+
+transdist(A,B,D):-
+        station(A,_,CA,1),
+        station(B,_,CB,1),
+        route(CA,CB,D).
+
+dfs(N, G, [N], 0,_):- N=G.
+dfs(N, G, [Z|T], Dist, Depth):-
+        Depth>0,
+        N\=G,
+        transdist(N,Z,D),
+        Depth1 is Depth-1,
+        dfs(Z,G,T,DT,Depth1),
+        Dist is D+DT.
+
+
+bfs([[Target|T]|_],Target,[Target|T]):-!.
+bfs([[X|T]|Ways], Target, S):-
+        Target\=X,
+        findall([Y,X|T],
+                (transdist(X,Y,_), \+ member(Y,[X|T])), L),
+        append(Ways,L, NWays),
+        bfs(NWays,Target,S).
+
+bf1([_-s(G,[Target|T]) |_],Target,s(G,[Target|T])):-!.
+bf1([_-s(G,[X|T])|Ways], Target, S):-
+        Target\=X,
+        findall(F1-s(G1,[Y,X|T]), after([X|T],G, Y,G1,F1), L),
+        append(L, Ways, NWays),
+        keysort(NWays,SNWays),
+        bf1(SNWays,Target,S).
+
+after([S|R],SG, T,TG,TG):-
+        transdist(S,T,D),
+        \+ member(T,[S|R]),
+        TG is SG + D.
+
+bf(Start, Target, Sol):-
+        bf1([0-s(0,[Start])], Target, Sol).
+
+create_route_table:-
+        sqlite_query(db, 'DROP TABLE IF EXISTS route', _),
+        sqlite_query(db, 'CREATE TABLE IF NOT EXISTS route (a int REFERENCES station (oid), b int REFERENCES station (oid), dist int)', _).
+
+fill_route_table(clear):-
+        sqlite_query(db, 'DELETE FROM route', _).
+
+fill_route_table:-
+        findroute(A,B),
+        % write('Route: '), write(A), write(' with '), write(B),nl,
+        shortest(A,B,_,D),
+        addroute(A,B,D),
+        format('Added: ~w with ~w dist: ~w\n', [A,B,D]),
+        fail.
+
+fill_route_table.
+
+addroute(A,B,D):-
+        \+ route(A,B,_),
+        sqlite_format_query(db, 'INSERT INTO route (a,b,dist) values (~w,~w,~w)'-[A,B,D],_).
+
+addroute(A,B,D):-
+        \+ route(B,A,_),
+        sqlite_format_query(db, 'INSERT INTO route (a,b,dist) values (~w,~w,~w)'-[B,A,D],_).
+
+findroute(A,B):-
+        station(_,_,A,1),
+        dist(N,A,_),
+        dist(N,B,_), B\=A,
+        station(_,_,B,1).
+
+sqlite_query_f(Conn, S-Args, ROW):-
+        swritef(Query, S, Args),
+        sqlite_query(Conn, Query, ROW).
+
+gstation(Guess, Name):-
+        sqlite_query_f(db, 'SELECT name FROM station where name LIKE \'%w%\''-[Guess], row(Name)). %
+
+gstation(Guess, Name, Transit):-
+        sqlite_query_f(db, 'SELECT name FROM station where name LIKE \'%w%\' and transit=%w'-[Guess,Transit], %
+                       row(Name)).
+
+        % select name from station where name LIKE 'Краснояр%' and transit=1;
