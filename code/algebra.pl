2a232369d50d218be37ced40c9c68e2811071565
Evgeny Cherkashin <eugene@irnok.net>
2015-01-21 00:47:23 +0800

    Going further in algebra.

--- code/algebra.pl
+++ code/algebra.pl
@@ -1,5 +1,25 @@
 
 
-d(Y,X,0):-var(Y),var(X),!.
+d(Y,X,0):-
+        var(Y),var(X),
+        numbervars(aTerm(Y,X), 0, _),
+        Y\=X,!.
 d(X,X,1):-var(X),!.
 d(C,_,0):-atom(C),!.
+d(N,_,0):-number(N),!.
+
+d(U+V,X,DU+DV):-!,
+        d(U,X,DU),
+        d(V,X,DV).
+
+d(U-V,X,DU-DV):-!,
+        d(U,X,DU),
+        d(V,X,DV).
+
+d(U*V,X,DU*V+DV*U):-!,
+        d(U,X,DU),
+        d(V,X,DV).
+
+d(U/V,X,(DU*V-DV*U)/(V^2)):-!,
+        d(U,X,DU),
+        d(V,X,DV).
